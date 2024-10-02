7b89c1f5cc752420f12c9be3ddc9628faf4b9102
Trần Ngọc Nhân <126933446+ngocnhan2k4@users.noreply.github.com>
2024-10-02 15:09:14 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-
+app.use(express.static(path.join(__dirname, '22120241_22120254_22120249')));
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, '22120241_22120254_22120249', 'default.html'));
 });
 
 app.listen(PORT, () => {
